9fc1e9dac6e7d79a663b63f67f16919b9cec37f2
abraz <abraz@absoluteinvest.com.br>
2024-12-05 18:04:27 -0300

    2024 3 part 1

--- 2024/3/main.cpp
+++ 2024/3/main.cpp
@@ -22,22 +22,81 @@ std::vector<std::string> parse_input(std::string input_file) {
     return lines;
 }
 
-std::vector<std::array<int, 2>> parse_lines(std::string line) {
-    int curr_pos = 0;
-    int curr_digit;
-    bool matching = false;
+std::vector<std::array<int, 2>> parse_line(std::string line, bool apply_cond = false) {
+    size_t curr_pos = 0;
+
     std::string mul_init = "mul(";
     std::vector<std::array<int, 2>> muls;
-    while (true){
-        size_t init_pos = line.find(mul_init);
-        if (init_pos == std::string::npos) break;
-        // TODO: draw the rest of the fucking owl
-        int next_digit = line.at(init_pos + mul_init.length());
-        if (isdigit(next_digit)){
-            curr_digit = (curr_digit * 10) + next_digit;
-            matching = true;
+    while (true) {
+        size_t init_pos = line.find(mul_init, curr_pos);
+        if (init_pos == std::string::npos) {
+            std::cout << "End of Line\n";
+            break;
+        } else {
+            std::cout << "Found 'mul(' at pos " << init_pos << '\n';
+            curr_pos = init_pos;
+        }
+
+        int curr_number = 0;
+        std::array<int, 2> mul;
+        while (true) {
+            char next_char = line.at(mul_init.length() + ++curr_pos - 1);
+            if (!((isdigit(next_char)) | (next_char == ',') |
+                  (next_char == ')'))) {
+                std::cout << "Broke out of while on char" << next_char
+                          << " at pos" << curr_pos - 1 << '\n';
+                break;
+            };
+
+            if (isdigit(next_char)) {
+                std::cout << "Found digit " << next_char << '\n';
+                int next_digit = next_char - '0';
+                curr_number = (curr_number * 10) + (next_digit);
+                continue;
+            }
+
+            if (next_char == ',') {
+                std::cout << "Found number delimiter, adding " << curr_number
+                          << " to arr\n";
+                mul[0] = curr_number;
+                curr_number = 0;
+                continue;
+            }
+
+            if (next_char == ')') {
+                std::cout << "Found mul delimiter, pushing mul(" << mul[0]
+                          << ',' << curr_number << ")' to vec\n";
+                mul[1] = curr_number;
+                muls.push_back(mul);
+                break;
+            }
         }
     }
+
+    return muls;
+}
+
+std::vector<std::array<int, 2>>
+get_mul_vectors(std::vector<std::string> lines) {
+    std::vector<std::array<int, 2>> result;
+
+    for (std::string line : lines) {
+        std::cout << "Parsing line\n\n" << line << "\n\n";
+        std::vector<std::array<int, 2>> parsed_line = parse_line(line);
+        for (std::array<int, 2> arr : parsed_line) {
+            result.push_back(arr);
+        }
+    }
+
+    return result;
+}
+
+int mul_vector(std::vector<std::array<int, 2>> muls) {
+    int result = 0;
+    for (std::array<int, 2> mul : muls) {
+        result += (mul[0] * mul[1]);
+    }
+    return result;
 }
 
 int main() {
@@ -49,7 +108,9 @@ int main() {
     std::cout << " -------------------------\n";
     std::cout << " ------SOLVE PART 1-------\n";
     std::cout << " -------------------------\n";
-    int part_1 = 0;
+
+    std::vector<std::array<int, 2>> muls = get_mul_vectors(lines);
+    int part_1 = mul_vector(muls);
 
     std::cout << " -------------------------\n";
     std::cout << " ------SOLVE PART 2-------\n";
