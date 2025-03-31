dd8fdd7fc9667e876bb38cdef765fa882e6c550e
abraz <abraz@absoluteinvest.com.br>
2025-03-31 15:51:17 -0300

    some ocaml project stuff

--- 2022/1/main.ml
+++ 2022/1/main.ml
@@ -1,2 +1,7 @@
+let read_file filename = In_channel.with_open_bin filename In_channel.input_all
+
 let () =
-  print_endline "Advent of Code 2022 - Day 1"
+    try
+      let contents = read_file "sample" in
+          Printf.printf "File contents: %s\n" contents
+    with Sys_error msg -> Printf.printf "Error opening file: %s\n" msg
--- _build/default/2022/1/main.ml
+++ _build/default/2022/1/main.ml
@@ -1,2 +1,6 @@
 let () =
-  print_endline "Advent of Code 2022 - Day 1"
+  print_endline
+    "Advent of Code 2022 - Day \
+     1isnfiasfhnqiafhnwidnwinwidnnkaefnqiowfnwifbwifbwifb sifhcwaifhcn asicw \
+     idhnwa iohcn ioashdioashj ciwcbiscnsicsichsnichicio oa jcwiopadj apopj \
+     caopsdj aopsdj "
